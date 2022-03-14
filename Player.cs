ab951811032a3d0da180a524bed62f57f16ab29b
Jason Cobb <jasoncobbatc@gmail.com>
2022-03-14 19:37:32 -0400

    Main adjusted to write to console with 3 options, including to exit the loop and save player names and data to the list

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿namespace firstConsoleApp_basketballStats
+{
+    internal class Player
+    {
+        public Player()
+        {
+
+        }
+           
+        public string? Name { get; set; }
+        public int ShotsAttempted { get; set; }
+
+        public int ShotsMade { get; set; }
+
+        public double percentage { get { return (Convert.ToDouble(ShotsMade) / Convert.ToDouble(ShotsAttempted)); } }
+
+        public bool Save()
+        { return true; 
+        }
+
+        public List<Player> Retrieve()
+        { return new List<Player>(); 
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,119 @@
 ﻿using System;
-
-class Program
+using System.Collections.Generic;
+namespace firstConsoleApp_basketballStats
 {
-    static string Ask(string question)
+    public class Program
     {
-        Console.Write(question + " ");
-        return Console.ReadLine();
+        static void Main(string[] args)
+        {
+            // Team team = new Team();
+            //Player player = new Player();
+            bool exitLoop = false;
+          
+            
+            List<Player> playerNames = new List<Player>();
+            
+
+            playerNames.Add(new Player
+            {
+                Name = "Jordan",
+                ShotsAttempted = 9,
+                ShotsMade = 5,
+
+            });
+            playerNames.Add(new Player
+            {
+                Name = "Bird",
+                ShotsAttempted = 7,
+                ShotsMade = 5,
+            });
+
+            playerNames.Add(new Player
+            {
+                Name = "Barkley",
+                ShotsAttempted = 5,
+                ShotsMade = 4
+            });
+
+
+            Console.WriteLine("Welcome to the sports statistics app!");
+            do
+            {
+                Console.WriteLine("Please Enter Menu Number Below");
+                Console.WriteLine("1 Enter Player Information");
+                Console.WriteLine("2 Display Player Information");
+                Console.WriteLine("3 Exit");
+                var menuSelection = Console.ReadLine();
+
+                switch(menuSelection){
+
+                    case "1":
+                        Player p = new Player();
+
+                        string lastName = AskName("What is the player's last name?");
+                        p.Name = lastName;
+
+                        string taken = Ask("How many shots have been taken?");
+                        p.ShotsAttempted = int.Parse(taken);
+                      
+                        string made = AskTwo("How many shots have been made?");
+                        p.ShotsMade = int.Parse(made);
+                      
+                        //return Console.ReadLine();
+                        Console.WriteLine($"The field goal percentage is {(p.percentage * 100).ToString("##.##")}%");
+
+                        playerNames.Add(p);
+
+                        break;
+                    case "2":
+                        Console.WriteLine($"There are {playerNames.Count} players on the team.");
+                        //foreach (Player in playerNames)
+                        Console.WriteLine($"Player: {playerNames}");
+
+                        break;
+                    case "3":
+                        exitLoop = true;
+                        break;
+
+                }
+
+
+            }
+            while (! exitLoop);
+        }
+
+
+        static string AskName(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine();
+        }
+
+        static string Ask(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine();
+        }
+
+        static string AskTwo(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine();
+        }
+
     }
-
-    static string AskTwo(string question)
-    {
-        Console.Write(question + " ");
-        return Console.ReadLine();
-    }
-
-
-    static int FieldGoalPercentage(int shotsMade, int shotsTaken)
-    {
-        return shotsMade / shotsTaken * 100;
-    }
-
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome to the sports statistics app!");
-        string shotsTaken = Ask("How many shots have been taken?");
-        //int number = int.Parse(entr);
-        Console.WriteLine(shotsTaken);
-        string shotsMade = AskTwo("How many shots have been made?");
-        Console.WriteLine(shotsMade);
-        Console.WriteLine($"The field goal percentage is {FieldGoalPercentage}%");
-    }
-    //Console.WriteLine(fieldGoalPercentage(shotsMade, shotsTaken));
-    //Console.WriteLine(fieldGoalPercentage(8, 10));
-
-    /*Static double freeThrowPercentage
-    Static double threePointPercentage
-    ?use
-    public interface IShotPercentage
-    {
-
-    }*/
-
 }
+
+//Console.WriteLine(fieldGoalPercentage(shotsMade, shotsTaken));
+//Console.WriteLine(fieldGoalPercentage(8, 10));
+
+//    Static double freeThrowPercentage
+//            Static double threePointPercentage
+//            ? use
+//            public interface IShotPercentage
+//    {
+
+//    }
+
+//}
+
