4ed09d438e757d5037eaee810cbd45ef8b8217ff
Jason Cobb <jasoncobbatc@gmail.com>
2022-03-21 19:02:53 -0400

    add detail of menu options to readme and on program added a line of spacing between the field goal percentage and the player stat lines as well as a line of space between the count of the number of players and the player stat line on the option 2 print to console.

--- Program.cs
+++ Program.cs
@@ -38,6 +38,7 @@ namespace firstConsoleApp_basketballStats
             Console.WriteLine("Welcome to the sports statistics app!");
             do
             {
+                Console.WriteLine("");
                 Console.WriteLine("Please Enter Menu Number and Press Return");
                 Console.WriteLine("1 Enter Player Information");
                 Console.WriteLine("2 Display Player Information");
@@ -57,15 +58,17 @@ namespace firstConsoleApp_basketballStats
                       
                         string made = AskTwo("How many shots have been made?");
                         p.ShotsMade = int.Parse(made);
-                      
-                   
+
+                        Console.WriteLine("");
                         Console.WriteLine($"The field goal percentage is {(p.percentage * 100).ToString("##.##")}%");
 
                         playerNames.Add(p);
 
                         break;
                     case "2":
+                        Console.WriteLine("");
                         Console.WriteLine($"There are {playerNames.Count} players on the team.");
+                        Console.WriteLine("");
 
                         foreach (Player player in playerNames)
                         {
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # firstConsoleApp-basketballStats
+This app will allow you to enter field goals taken and field goals made to calculate a field goal percentage for players entered for sports such as basketball that use the verbage of field goals and shots made.
+This console app will present on the screen 3 opportunities. 1. will give the chance to add a player name and then enter their statistics of field goals attempted and made which will produce the field goal percentage. You may do this for another player by repeating the option 1 after hitting return on the previous player.
+Option 2. Will display the information of the players on the team, which will include 3 preset players as well as any that are entered in the console by step one.  The number of shots made out of number taken will be shown as well as the field goal percentage will be calculated behind the scenes and displayed for each player. The total number of players is also counted and produced on this line, which will be the 3 pre-set players as well as any additional players entered.
+Option 3. will allow you to exit the console app, then hit the spacebar to close the window.
+
